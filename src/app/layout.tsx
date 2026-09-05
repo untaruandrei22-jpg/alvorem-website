75772b5a440afe4afb0e50d83d97fee068b2a234
untaruandrei <untaruandrei22@gmail.com>
2026-09-05 15:55:17 +0300

    Enable ALVOREM fidelity layer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Cormorant_Garamond, Manrope } from "next/font/google";
 import "./globals.css";
+import "./fidelity.css";
 
 const manrope = Manrope({
   subsets: ["latin"],

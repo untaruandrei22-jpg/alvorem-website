3ddaacf9248ae6c7ddc1bc1c3692cfbfa2dae37d
untaruandrei22-jpg <241479990+untaruandrei22-jpg@users.noreply.github.com>
2026-09-08 01:46:36 +0300

    feat: add Alvorem coming soon page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,36 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: "/about",
+        destination: "/",
+        permanent: false,
+      },
+      {
+        source: "/careers",
+        destination: "/",
+        permanent: false,
+      },
+      {
+        source: "/pricing",
+        destination: "/",
+        permanent: false,
+      },
+      {
+        source: "/solutions",
+        destination: "/",
+        permanent: false,
+      },
+      {
+        source: "/work",
+        destination: "/",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
